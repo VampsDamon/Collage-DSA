fd59d940b68e0f960a0af3d7bb289cdd03c23a0e
VampsDamon <91468616+VampsDamon@users.noreply.github.com>
2022-09-09 09:12:09 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,26 @@
+//Address of arrays in rows majaor
+#include<stdio.h>
+int main()
+{
+  int n,m,I,J,i,j;
+  printf("Enter size of row and column :- ");
+  scanf("%d%d",&n,&m);
+  int arr[n][m];
+  printf("Enter array elements :- ");
+    for( i=0;i<n;i++)
+      for( j=0;j<m;j++)
+       scanf("%d",&arr[i][j]);
+printf(" Array elements in rows order :- \n");
+    for( i=0;i<n;i++){
+      for( j=0;j<m;j++)
+       printf("%d\t",arr[i][j]);
+     printf("\n");
+}
+
+  printf("Enter index of Number to find it's address :-");
+  scanf("%d%d",&I,&J);
+int add,b=0,lr=0,lc=0;
+add=b+sizeof(arr[0][0])*((I-lr)*m + (J-lc));
+  printf("Address of array :- %d \n",add);
+  return 0;
+}
