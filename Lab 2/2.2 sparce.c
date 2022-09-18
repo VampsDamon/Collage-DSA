e844028c08524a93df1267cab37535af247ff69c
VampsDamon <91468616+VampsDamon@users.noreply.github.com>
2022-09-18 21:57:47 +0530

    Update 2.2 sparce.c

--- Lab 2/2.2 sparce.c
+++ Lab 2/2.2 sparce.c
@@ -2,35 +2,38 @@
 int main()
 {
     int nz = 0, i, j, m, n;
+    int k = -1;
     printf("Enter size of row and column :- ");
     scanf("%d%d", &n, &m);
     int arr[n][m];
     printf("Enter array elements :- ");
     for (i = 0; i < n; i++)
     {
-        for (j = 0; j < m; j++)
+        for (j = 0; j < m; j++){
             scanf("%d", &arr[i][j]);
         if (arr[i][j] != 0)
-            nz++;
+            nz++;}
     }
+    printf("nz=%d",nz);
     int nzz[nz][3];
-    int k = 0;
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < m; j++)
         {
             if (arr[i][j] != 0)
             {
-                nzz[k][0] = arr[i][j];
-                nzz[k][1] = i;
-                nzz[k][2] = j;
                 k++;
+                nzz[k][0] = i;
+                nzz[k][1] = j;
+                nzz[k][2] = arr[i][j];
+                
             }
+            
         }
     }
     printf(" Elements of sparse Matrix :- \n");
-    printf("Element  Rows  Columns\n");
-    for (i = 0; i < k; i++)
+    printf("Rows  Columns  Elements\n");
+    for (i = 0; i <= k; i++)
     {
         for (j = 0; j < 3; j++)
             printf(" %d   \t", nzz[i][j]);
